30d300fb3fc628e7dcf14acb1cd15c7e119eabea
Tim Phillips <timdphillips@gmail.com>
2020-01-26 16:40:52 +0000

    Add a couple more images

--- index.js
+++ index.js
@@ -1,52 +1,49 @@
-window.addEventListener("DOMMouseScroll", onMouseWheel, false);
-window.onmousewheel = document.onmousewheel = onMouseWheel; // IE
+function init() {
+  window.addEventListener("DOMMouseScroll", onMouseWheel, false);
+  window.onmousewheel = document.onmousewheel = onMouseWheel; // IE
 
-const image1 = window.document.getElementById("image1");
-const image2 = window.document.getElementById("image2");
-const image3 = window.document.getElementById("image3");
-const image4 = window.document.getElementById("image4");
-
-image1.style.height = "800px";
-image1.style.opacity = 1;
-
-image2.style.height = "800px";
-image2.style.opacity = 1;
-
-image3.style.height = "800px";
-image3.style.opacity = 1;
-
-image4.style.height = "800px";
-image4.style.opacity = 1;
-
-let count = 0;
-function onMouseWheel(event) {
-  const scroll = event.deltaY * -1 || event.wheelDelta || event.detail * -1;
-
-  const image1Opacity = image1.style.opacity;
-  const image2Opacity = image2.style.opacity;
-  const image3Opacity = image3.style.opacity;
-
-  if (scroll > 0) {
-    count++;
-  } else {
-    count--;
+  const imageIds = ["image1", "image2", "image3", "image4", "image5", "image6"];
+  const images = imageIds.map(id => window.document.getElementById(id));
+  for (const image of images) {
+    image.style.opacity = 1;
+    image.style.filter = "blur(0px)";
+    image.style.height = "800px";
+    image.style.display = "inherit";
   }
 
-  if (scroll > 0) {
-    if (count >= 20 && count < 40) {
-      image1.style.opacity = image1Opacity - 0.05;
-    } else if (count >= 60 && count < 80) {
-      image2.style.opacity = Number(image2Opacity) - 0.05;
-    } else if (count >= 100 && count < 120) {
-      image3.style.opacity = Number(image3Opacity) - 0.05;
+  let tick = 0;
+  const ticksPerImage = 30;
+
+  function onMouseWheel(event) {
+    const scroll = event.deltaY * -1 || event.wheelDelta || event.detail * -1;
+
+    const imageIndex = Math.floor(tick / ticksPerImage);
+    if (tick < 0 || imageIndex > images.length - 1) {
+      return;
     }
-  } else {
-    if (count >= 20 && count < 40) {
-      image1.style.opacity = Number(image1Opacity) + 0.05;
-    } else if (count >= 60 && count < 80) {
-      image2.style.opacity = Number(image2Opacity) + 0.05;
-    } else if (count >= 100 && count < 120) {
-      image3.style.opacity = Number(image3Opacity) + 0.05;
+
+    const image = images[imageIndex];
+    const ticksWithinImage = tick - imageIndex * ticksPerImage;
+
+    // blur the image
+    if (ticksWithinImage >= 17 && ticksWithinImage < 23) {
+      const blur = Number(
+        image.style.filter.replace("blur(", "").replace("px)", "")
+      );
+      image.style.filter = `blur(${blur + (scroll > 0 ? 0.5 : -0.5)}px)`;
+      image.style.height = `${image.height + (scroll > 0 ? 1 : -1)}px`;
+    }
+
+    // fade out the image
+    if (ticksWithinImage >= 20 && ticksWithinImage <= ticksPerImage) {
+      const opacity = Number(image.style.opacity);
+      image.style.opacity = opacity + (scroll > 0 ? -0.1 : 0.1);
+    }
+
+    if (scroll > 0) {
+      tick++;
+    } else if (tick > 0) {
+      tick--;
     }
   }
 }
