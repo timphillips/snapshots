f868cdea4f0bc63688e1832badcc4d98d20448dc
Tim Phillips <timdphillips@gmail.com>
2020-01-26 17:19:43 +0000

    Start to refine the aesthetic

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ function init() {
 
   const imageIds = ["image1", "image2", "image3", "image4", "image5", "image6"];
   const images = imageIds.map(id => window.document.getElementById(id));
+
+  // initial settings
   for (const image of images) {
-    image.style.opacity = 1;
-    image.style.filter = "blur(0px)";
-    image.style.height = "800px";
+    image.style.opacity = 0;
+    image.style.filter = "blur(5px)";
+    image.style.height = "700px";
     image.style.display = "inherit";
   }
 
   let tick = 0;
-  const ticksPerImage = 30;
+  const ticksPerImage = 40;
 
   function onMouseWheel(event) {
     const scroll = event.deltaY * -1 || event.wheelDelta || event.detail * -1;
@@ -25,17 +27,31 @@ function init() {
     const image = images[imageIndex];
     const ticksWithinImage = tick - imageIndex * ticksPerImage;
 
-    // blur the image
-    if (ticksWithinImage >= 17 && ticksWithinImage < 23) {
+    // fade in
+    if (ticksWithinImage < 10) {
+      const opacity = Number(image.style.opacity);
+      image.style.opacity = opacity + (scroll > 0 ? 0.1 : -0.1);
+    }
+    if (ticksWithinImage >= 5 && ticksWithinImage < 15) {
+      const blur = Number(
+        image.style.filter.replace("blur(", "").replace("px)", "")
+      );
+      image.style.filter = `blur(${blur + (scroll > 0 ? -0.5 : 0.5)}px)`;
+
+      const height = Number(image.style.height.replace("px", ""));
+      image.style.height = `${height + (scroll > 0 ? -1 : 1)}px`;
+    }
+
+    // fade out
+    if (ticksWithinImage >= 27 && ticksWithinImage < 33) {
       const blur = Number(
         image.style.filter.replace("blur(", "").replace("px)", "")
       );
       image.style.filter = `blur(${blur + (scroll > 0 ? 0.5 : -0.5)}px)`;
-      image.style.height = `${image.height + (scroll > 0 ? 1 : -1)}px`;
+      const height = Number(image.style.height.replace("px", ""));
+      image.style.height = `${height + (scroll > 0 ? 1 : -1)}px`;
     }
-
-    // fade out the image
-    if (ticksWithinImage >= 20 && ticksWithinImage <= ticksPerImage) {
+    if (ticksWithinImage >= 30 && ticksWithinImage <= ticksPerImage) {
       const opacity = Number(image.style.opacity);
       image.style.opacity = opacity + (scroll > 0 ? -0.1 : 0.1);
     }
